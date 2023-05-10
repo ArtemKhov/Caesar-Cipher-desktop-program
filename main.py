2058525ab75d51ceb93b7bb672220faa7e75e593
Artem <artem.kotyakhov@gmail.com>
2023-05-10 22:33:44 +0300

    main-0004: initial tkinter window. Add logo. Add combobox in test version.

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
-import tkinter
+from tkinter import *
+from tkinter import ttk
 from alphabet import Alphabet
+
+LANGUAGES = ["English", "Russian"]
 
 english_alphabet = Alphabet("english.txt")
 
 a = english_alphabet.encrypt_text("text_encr.txt", 2, 'encode')
+
+def combo_click(event):
+    if combobox.get() == "English":
+        myLabel = Label(text=a)
+        myLabel.grid(column=1, row=3)
+
+
+windows = Tk()
+windows.title("Caesar Cipher")
+windows.config(padx=100, pady=5)
+
+# Logo
+canvas = Canvas(width=200,height=200)
+logo_image = PhotoImage(file="img/caesar-cipher.png")
+canvas.create_image(100, 100, image=logo_image)
+canvas.grid(column=1, row=0)
+
+# Combobox
+ttk.Label(windows, text="Select cipher language:").grid(column=1, row=1)
+combobox = ttk.Combobox(values=LANGUAGES)
+combobox.bind("<<ComboboxSelected>>", combo_click)
+combobox.grid(column=1, row=2)
+
+# Button
+
+
+windows.mainloop()
 
 
 
