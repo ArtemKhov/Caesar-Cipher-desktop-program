e49ab6a70d2649316fc840682eddaab332500458
Artem <artem.kotyakhov@gmail.com>
2023-05-11 21:49:34 +0300

    main-0007: get value from scale

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ a = english_alphabet.encrypt_text("text_encr.txt", 2, 'decode')
 shift = english_alphabet.count_letter_in_alphabet
 print(shift)
 
+windows = Tk()
+windows.title("Caesar Cipher")
+windows.config(padx=100, pady=5)
+
 # Set value when user choose one of the languages
 def combo_click(event):
     if combobox.get() == "English":
@@ -23,10 +27,12 @@ def radiobutton_click():
         myLabel = Label(text="Decode")
         myLabel.grid(column=4, row=3)
 
-
-windows = Tk()
-windows.title("Caesar Cipher")
-windows.config(padx=100, pady=5)
+# Get value from Scale of the shift
+def get_scale_value(event):
+    # TODO: if scale == 0 popup alert that need to select shift differ from 0
+    scale_value = str(scale_variable.get())
+    myLabel = Label(text=scale_value)
+    myLabel.grid(column=1, row=6)
 
 # Logo
 canvas = Canvas(width=200,height=200)
@@ -51,12 +57,14 @@ decode_radiobutton = Radiobutton(text="Decode", variable=radiobutton, value=1, c
 decode_radiobutton.grid(column=3, row=3)
 
 # Scale: shift amount
+scale_variable = Variable()
+shift_scale = Scale(variable=scale_variable, from_=0, to=shift, orient=HORIZONTAL, command=get_scale_value)
+shift_scale.grid(column=1, row=4)
+
 scale_label = Label(text="Set cipher shift")
 scale_label.grid(column=1, row=5)
 
-shift_scale = Scale(from_=1, to=shift,
-                    orient=HORIZONTAL)
-shift_scale.grid(column=1, row=4)
+
 
 # Button
 
