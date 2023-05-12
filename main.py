8f369c93e187cee83b870354f4dfd930f9f9062c
Artem <artem.kotyakhov@gmail.com>
2023-05-12 23:18:15 +0300

    main-0009: add Button: Encode/Decode. Replace language files in folder

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ from tkinter import filedialog
 
 LANGUAGES = ["English", "Russian"]
 
-english_alphabet = Alphabet("english.txt")
-
-a = english_alphabet.encrypt_text("text_encr.txt", 2, 'decode')
+english_alphabet = Alphabet('alphabets/english.txt')
 shift = english_alphabet.count_letter_in_alphabet
-print(shift)
+english_cipher = english_alphabet.encrypt_text("text_encr.txt", 2, 'decode')
+
 
 windows = Tk()
 windows.title("Caesar Cipher")
@@ -20,14 +19,20 @@ text_editor = Text()
 # Set value when user choose one of the languages
 def combo_click(event):
     if combobox.get() == "English":
-        myLabel = Label(text="OK")
-        myLabel.grid(column=0, row=2)
+        english_alphabet = Alphabet("alphabets/english.txt")
+        return english_alphabet
+    elif combobox.get() == "Russian":
+        russian_alphabet = Alphabet("alphabets/russian.txt")
+        return russian_alphabet
+
 
 # Set value when user choose decode option
 def radiobutton_click():
     if radiobutton.get() == 1:
-        myLabel = Label(text="Decode")
-        myLabel.grid(column=4, row=3)
+        decrypt = Label(text="Decrypt")
+        decrypt.grid(column=4, row=3)
+        return "decode"
+    return 'encode'
 
 # Get value from Scale of the shift
 def get_scale_value(event):
@@ -35,6 +40,7 @@ def get_scale_value(event):
     scale_value = str(scale_variable.get())
     myLabel = Label(text=scale_value)
     myLabel.grid(column=1, row=6)
+    return int(scale_value)
 
 # Open and read file
 def open_file():
@@ -46,6 +52,7 @@ def open_file():
             text_editor.insert("1.0", text)
             myLabel = Label(text="File downloaded successfully")
             myLabel.grid(column=1, row=8)
+            return text_editor
 
 # Save the final file
 def save_file():
@@ -54,6 +61,17 @@ def save_file():
         text = text_editor.get("1.0", END)
         with open(filepath, "w") as file:
             file.write(text)
+
+# Switch state of the "Save file" button
+def switch():
+    if save_button["state"] == "disabled":
+        save_button["state"] = "normal"
+
+
+# Encode/Decode function
+def encrypt():
+    return Alphabet.encrypt_text(open_file, get_scale_value, radiobutton_click())
+
 
 # Logo
 canvas = Canvas(width=200,height=200)
@@ -66,6 +84,8 @@ ttk.Label(windows, text="Select cipher language:").grid(column=1, row=1)
 combobox = ttk.Combobox(values=LANGUAGES)
 combobox.bind("<<ComboboxSelected>>", combo_click)
 combobox.grid(column=1, row=2)
+
+alphabet = combo_click(combobox)
 
 # Radiobutton: Encode and Decode
 radiobutton = BooleanVar()
@@ -86,13 +106,17 @@ scale_label = Label(text="Set cipher shift")
 scale_label.grid(column=1, row=5)
 
 # Button: Open file
-open_button = open_button = Button(text='Open file', command=open_file)
+open_button = Button(text='Open file', command=open_file)
 open_button.grid(column=1, row=7)
 
+# Button: Encode/Decode
+encrypt_button = Button(text="Encode/Decode", command=lambda: [switch(),
+                                                               encrypt()])
+encrypt_button.grid(column=1, row=9)
+
 # Button: Save File
-# TODO: must be inactive while user don't encrypt file
-save_button = Button(text='Save file', command=save_file)
-save_button.grid(column=1, row=9)
+save_button = Button(text='Save file', state="disabled", command=save_file)
+save_button.grid(column=1, row=10)
 
 
 windows.mainloop()
